c1abcb2234b60740a386c865fae581bd56dcd872
philippineglobaloutsourcing <philippineoutsourcing@gmail.com>
2015-12-30 09:11:03 +0800

    Forms, UI, Charts, Tables Update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,27 +31,93 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'templates'], function () {
 	Route::get('/', function(){
-		return view('__adminlte.pages.dashboard');
+		return view('__adminlte.demos.dashboard');
 	});
 	Route::get('dashboard', function(){
-		return view('__adminlte.pages.dashboard');
+		return view('__adminlte.demos.dashboard');
 	});
 	Route::get('dashboard1', function(){
-		return view('__adminlte.pages.dashboard1');
+		return view('__adminlte.demos.dashboard1');
 	});
 	Route::get('widgets', function(){
-		return view('__adminlte.pages.widgets');
+		return view('__adminlte.demos.widgets');
 	});
 });
 
 Route::group(['prefix' => 'templates/charts'], function () {
 	Route::get('/', function(){
-		return view('__adminlte.pages.charts.chartjs');
+		return view('__adminlte.demos.charts.chartjs');
 	});
 	Route::get('chartjs', function(){
-		return view('__adminlte.pages.charts.chartjs');
+		return view('__adminlte.demos.charts.chartjs');
+	});
+	Route::get('morris', function(){
+		return view('__adminlte.demos.charts.morris');
+	});
+	Route::get('flot', function(){
+		return view('__adminlte.demos.charts.flot');
+	});
+	Route::get('inline', function(){
+		return view('__adminlte.demos.charts.inline');
 	});
 });
+
+Route::group(['prefix' => 'templates/ui'], function () {
+	Route::get('/', function(){
+		return view('__adminlte.demos.ui.general');
+	});
+	Route::get('general', function(){
+		return view('__adminlte.demos.ui.general');
+	});
+	Route::get('icons', function(){
+		return view('__adminlte.demos.ui.icons');
+	});
+	Route::get('icons/{icon}', function($icon){
+		return view('__adminlte.demos.ui.icon', compact('icon'));
+	});
+	Route::get('buttons', function(){
+		return view('__adminlte.demos.ui.buttons');
+	});
+	Route::get('sliders', function(){
+		return view('__adminlte.demos.ui.sliders');
+	});
+	Route::get('timeline', function(){
+		return view('__adminlte.demos.ui.timeline');
+	});
+	Route::get('modals', function(){
+		return view('__adminlte.demos.ui.modals');
+	});
+});
+
+
+Route::group(['prefix' => 'templates/forms'], function () {
+	Route::get('/', function(){
+		return view('__adminlte.demos.forms.general');
+	});
+	Route::get('general', function(){
+		return view('__adminlte.demos.forms.general');
+	});
+	Route::get('advanced', function(){
+		return view('__adminlte.demos.forms.advanced');
+	});
+	Route::get('editors', function(){
+		return view('__adminlte.demos.forms.editors');
+	});
+});
+
+Route::group(['prefix' => 'templates/tables'], function () {
+	Route::get('/', function(){
+		return view('__adminlte.demos.tables.simple');
+	});
+	Route::get('simple', function(){
+		return view('__adminlte.demos.tables.simple');
+	});
+	Route::get('data', function(){
+		return view('__adminlte.demos.tables.data');
+	});
+});
+
+
 
 Route::group(['middleware' => ['web']], function () {
     //
